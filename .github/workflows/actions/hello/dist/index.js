a21bae51db4e66084e42d051d52e1d8be90389eb
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-04-11 12:00:00 +0100

    Updating dist

--- .github/workflows/actions/hello/dist/index.js
+++ .github/workflows/actions/hello/dist/index.js
@@ -6054,6 +6054,9 @@ try {
   
   const time = new Date()
   core.setOutput("time", time.toTimeString())
+
+  const town = "Springfield, USA"
+  core.setOutput("town", town.toString())
   
   console.log("Let's have a look at what's in the github object")
   console.log(JSON.stringify(github, null, "\t"))
