8d43b6ece5a1ccac71554c0b55b4a73404b37f4c
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-04-11 11:45:11 +0100

    Adding an output

--- .github/workflows/actions/hello/index.js
+++ .github/workflows/actions/hello/index.js
@@ -11,6 +11,8 @@ try {
   
   const time = new Date()
   core.setOutput("time", time.toTimeString())
+
+  core.setOutput("town", "Springfield, USA")
   
   console.log("Let's have a look at what's in the github object")
   console.log(JSON.stringify(github, null, "\t"))
